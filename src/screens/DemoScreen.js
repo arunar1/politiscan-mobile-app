b746416502d3e13d48d4caa4faa0268a9e06514c
Arun <arunarofficial00@gmail.com>
2024-02-02 14:01:44 +0530

    addes them demo sentiment fetching page

--- src/screens/DemoScreen.js
+++ src/screens/DemoScreen.js
@@ -11,8 +11,10 @@ const DemoScreen = () => {
         const response = await axios.post("http://192.168.0.116:4000/api/sentiment", {
           data: inputText,
         });
+
+        console.log(response.data)
     
-        const sentiment = response.data.sentiment;
+        const sentiment = response.data.calculatedsentiment;
         setSentimentResult(sentiment);
       } catch (error) {
         console.error("Error fetching sentiment:", error);
@@ -22,13 +24,14 @@ const DemoScreen = () => {
 
   return (
     <View style={{ padding: 20, flex: 1, justifyContent: "center" }}>
+        <Text style={{paddingBottom:30,fontSize:18,}}>Please share your comments on project :</Text>
       <TextInput
         placeholder="Enter your data"
         onChangeText={(text) => setInputText(text)}
         value={inputText}
         style={{ height: 40, borderColor: "gray", borderWidth: 1, marginBottom: 20 }}
       />
-      <Button title="Submit" onPress={handleSubmit} />
+      <Button title="Submit"  style={styles.b} color={'purple'}  onPress={handleSubmit} />
 
       {sentimentResult !== null && (
         <View style={{ marginTop: 20 }}>
@@ -48,6 +51,9 @@ const styles=StyleSheet.create({
     },
     textresult:{
         color:'red'
+    },
+    button:{
+        paddingTop:55,
     }
 
 })
