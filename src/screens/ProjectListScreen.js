5e3a90fda01717ed29f1fa564570fee15e2bd480
Arun <arunarofficial00@gmail.com>
2024-03-19 00:19:18 +0530

    responded project highlight

--- src/screens/ProjectListScreen.js
+++ src/screens/ProjectListScreen.js
@@ -8,6 +8,8 @@ import { MaterialCommunityIcons } from '@expo/vector-icons';
 const ProjectListScreen = ({ navigation,route }) => {
 
   const [details,setDetails]=useState([])
+  const [result,setResult]=useState([]);
+
 
   
   const {data} = route.params
@@ -19,7 +21,7 @@ const ProjectListScreen = ({ navigation,route }) => {
           constituency: data.constituency
         });
 
-        console.log(response)
+        
         if (response.status === 200) {
           const { projects } = response.data;
           if (projects && projects.length > 0) {
@@ -38,10 +40,30 @@ const ProjectListScreen = ({ navigation,route }) => {
     };
 
     fetchProjects();
+    checkFeedback();
   }, []);
 
 
+  const checkFeedback = async () => {
+    try {
+      const response = await axios.post(`${Api.API_BACKEND}/project/AllprojectSentimentCheck`, {
+        sentimentData: {
+          aadharNo: data.aadharNo, 
+        },
+        constituency:data.constituency
+      });
+
+
+      setDetails(response.data.projectDetails)
+      
+    } catch (error) {
+      Alert.alert('Error', 'Please try again.');
+    }
+  };
+
  
+
+  console.log(details)
 
 
   
@@ -56,26 +78,31 @@ const ProjectListScreen = ({ navigation,route }) => {
 
   return (
     <View style={styles.container}>
-      <FlatList
-        data={projects}
-        keyExtractor={(item) => item.projectId.toString()} 
-        renderItem={({ item }) => (
-          <TouchableOpacity
-    style={styles.projectItem}
-    onPress={() => navigation.navigate('projectdetails', { item: item, data: data })}
->
-    <View>
-        <Text style={styles.projectTitle}>{item.projectId}</Text>
-        <Text style={styles.projectTitle}>{item.projectName.trim()}</Text>
-    </View>
-    {data.userType === 'admin' ? (
-        <MaterialCommunityIcons name="delete" size={24} color="black" onPress={deleteProject} />
-    ) : null}
-</TouchableOpacity>
+     <FlatList
+  data={projects}
+  keyExtractor={(item) => item.projectId.toString()} 
+  renderItem={({ item }) => {
+    const hasSentimentData = details.some(detail => {
+      return detail.projectId === item.projectId && detail.sentimentData.some(data => data.aadharNo === data.aadharNo);
+    });
 
+    return (
+      <TouchableOpacity
+        style={[styles.projectItem, { backgroundColor: hasSentimentData ? '#c0e6ff' : '#fff' }]}
+        onPress={() => navigation.navigate('projectdetails', { item: item, data: data })}
+      >
+        <View>
+          <Text style={styles.projectTitle}>{item.projectId}</Text>
+          <Text style={styles.projectTitle}>{item.projectName.trim()}</Text>
+        </View>
+        {data.userType === 'admin' ? (
+          <MaterialCommunityIcons name="delete" size={24} color="black" onPress={deleteProject} />
+        ) : null}
+      </TouchableOpacity>
+    );
+  }}
+/>
 
-        )}
-      />
     </View>
   );
 };
