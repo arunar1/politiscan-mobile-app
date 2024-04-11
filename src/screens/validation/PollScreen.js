fc8a7fe95b3a95bf30d1441ece6ca30a31122c4f
Arun <arunarofficial00@gmail.com>
2024-04-11 17:48:33 +0530

    feat : added a new filtering based on district and constituency

--- src/screens/validation/PollScreen.js
+++ src/screens/validation/PollScreen.js
@@ -1,47 +1,121 @@
-import React, { useState,useEffect } from 'react';
-import { View, StyleSheet, TextInput, Button, Alert,FlatList,TouchableOpacity,Text } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, StyleSheet, TextInput, Button, Alert, FlatList, TouchableOpacity, Text } from 'react-native';
 import axios from 'axios';
 import { Api } from '../../constants';
 import { MaterialCommunityIcons } from '@expo/vector-icons';
+import { Picker } from '@react-native-picker/picker';
+import { setWidth } from '../../utils';
 
-const PollScreen = ({navigation,route}) => {
+const PollScreen = ({ navigation, route }) => {
     const [pollDescription, setPollDescription] = useState('');
-
-let constituency=''
-let userType=''
-    if (route && route.params) {
-       const{data}= route.params;
-       constituency=data.constituency
-       userType=data.userType;
-    } 
-
-
-
-    const [deletedItem,setDeletedItem]=useState('')
-
+    const [constituencies, setConstituencies] = useState([]);
+    const [selectedConstituency, setSelectedConstituency] = useState('');
+    const [districts, setDistricts] = useState([]);
+    const [selectedDistrict, setSelectedDistrict] = useState('');
+    const [deletedItem, setDeletedItem] = useState('');
     const [poll, setPoll] = useState([]);
+    const [filteredPoll, setFilteredPoll] = useState([]);
+
+    let constituency = '';
+    let userType = '';
+    let dist=''
+    
+    if (route && route.params) {
+        const{ data } = route.params;
+        constituency = data.constituency;
+        userType = data.userType;
+        dist=data.district
+        
+
+    }
+
+    
 
     useEffect(() => {
         getPoll();
     }, [pollDescription,deletedItem]);
 
+    useEffect(() => {
+        if (poll.length > 0) {
+            const uniqueDistricts = [...new Set(poll.map(item => item.district))];
+            setDistricts(uniqueDistricts);
+        }
+    }, [poll,deletedItem,pollDescription]);
+
+    
+
+    useEffect(() => {
+        if (selectedDistrict) {
+            const filteredConstituencies = poll
+                .filter(item => {
+                    return(item.district == selectedDistrict)
+                })
+                .map(item => {
+                    return(
+                        item.constituency
+                    )
+                });
+            const uniqueConstituencies = [...new Set(filteredConstituencies)];
+            setConstituencies(uniqueConstituencies);
+
+            
+            filterData(selectedConstituency, selectedDistrict);
+        }
+    }, [selectedDistrict,selectedConstituency,deletedItem]);
+
+    useEffect(() => {
+        if (selectedDistrict || selectedConstituency) {
+            filterData(selectedConstituency, selectedDistrict);
+        } else {
+            setFilteredPoll(poll); // Reset filtered data if no filters are applied
+        }
+    }, [selectedConstituency, selectedDistrict, deletedItem, pollDescription, poll]);
+    
+
+    console.log("hi",deletedItem)
+
+   
+
     const getPoll = async () => {
         try {
             const response = await axios.get(`${Api.API_BACKEND}/getpoll`, {});
-            console.log(response.data.data)
-            if(response.data){
+            if (response.data) {
                 setPoll(response.data.data.reverse());
             }
         } catch (error) {
-            console.error('Error fetching poll:', error.message); 
+            console.error('Error fetching poll:', error.message);
         }
     };
-    
-    console.log(poll)
 
-    const handlePollPress =(pollItem)=>{
-            navigation.navigate('pollresultscreen',{item:pollItem})
-    }
+    const filterData = (selectedConstituency, selectedDistrict) => {
+        let filteredResults = poll;
+
+        if (selectedDistrict) {
+            filteredResults = filteredResults.filter(record =>
+                record.district == selectedDistrict
+            );
+        }
+        console.log(selectedConstituency)
+
+        if (selectedConstituency) {
+            filteredResults = filteredResults.filter(record =>
+                {
+                    console.log('geo',record.constituency,selectedConstituency)
+                    return(
+                        record.constituency == selectedConstituency
+                    )
+                }
+            );
+        }
+
+        console.log(filteredResults)
+
+        setFilteredPoll(filteredResults);
+    };
+
+    const handlePollPress = (pollItem) => {
+        navigation.navigate('pollresultscreen', { item: pollItem });
+    };
 
     const deleteItem = async (item) => {
         Alert.alert(
@@ -60,7 +134,8 @@ let userType=''
                         try {
                             const response = await axios.delete(`${Api.API_BACKEND}/deletePoll`, { data: { description: item } });
                             console.log(response.data);
-                            setDeletedItem(response.data.message);
+                            setDeletedItem(response.data);
+                            Alert.alert('Delete',response.data.message)
                         } catch (error) {
                             console.error('Error deleting poll:', error);
                         }
@@ -70,43 +145,34 @@ let userType=''
             { cancelable: false }
         );
     };
-    
 
     const renderPollItem = ({ item }) => (
-        userType=='admin' && item.constituency === constituency ? (
+        userType === 'admin' && item.constituency === constituency ? (
             <TouchableOpacity style={styles.pollItem} onPress={() => handlePollPress(item)}>
-            
-            <View>
-               {/* <Text style={{fontFamily:'Bold',marginBottom:10}}>{item.constituency.length!=0 ? <Text>Constituency : {item.constituency}</Text>:null}</Text> */}
-               <Text style={{fontFamily:'Regular',marginBottom:10}} >{item.date}</Text>
-
-                <Text style={{fontFamily:'Regular'}} >{item.description}</Text>
-
-               </View>
-                
+                <View>
+                    <Text style={{ fontFamily: 'Regular', marginBottom: 10 }}>{item.date}</Text>
+                    <Text style={{ fontFamily: 'Regular' }}>{item.description}</Text>
+                </View>
                 <MaterialCommunityIcons name="delete" size={30} color="black" onPress={() => deleteItem(item.description)} />
             </TouchableOpacity>
-        ) : userType!='admin'?((
+        ) : userType !== 'admin' ? (
             <TouchableOpacity style={styles.pollItem} onPress={() => handlePollPress(item)}>
-               <View>
-               <Text style={{fontFamily:'Bold',marginBottom:10}}>{item.constituency!="nodata" ? <Text>Constituency : {item.constituency}</Text>:null}</Text>
-               <Text style={{fontFamily:'Regular',marginBottom:10}} >{item.date}</Text>
-
-                <Text style={{fontFamily:'Regular'}} >{item.description}</Text>
-
-               </View>
+                <View>
+                    <Text style={{ fontFamily: 'Bold', marginBottom: 10 }}>{item.constituency !== "Admin" ? <Text>Constituency : {item.constituency}</Text> : null}</Text>
+                    <Text style={{ fontFamily: 'Regular', marginBottom: 10 }}>{item.date}</Text>
+                    <Text style={{ fontFamily: 'Regular' }}>{item.description}</Text>
+                </View>
                 <MaterialCommunityIcons name="delete" size={30} color="black" onPress={() => deleteItem(item.description)} />
             </TouchableOpacity>
-        )):null
+        ) : null
     );
-    
 
     const handleAddPoll = async () => {
         try {
             const response = await addPoll(pollDescription);
             if (response.status === 200) {
                 Alert.alert('Success', 'Poll added successfully');
-                setPollDescription('')
+                setPollDescription('');
             } else {
                 throw new Error('Failed to add poll');
             }
@@ -116,27 +182,22 @@ let userType=''
         }
     };
 
-    const pollvalidation =()=>{
-        console.log(pollDescription)
-        if(!pollDescription.trim()){
-            Alert.alert("info",'Discription is empty')
-            return false
+    const pollvalidation = () => {
+        if (!pollDescription.trim()) {
+            Alert.alert("info", 'Description is empty');
+            return false;
+        } else {
+            return true;
         }
-        else{
-            return true
-        }
-    }
-
-    
-
-    const addPoll = async (description) => {
-        return await axios.post(`${Api.API_BACKEND}/addpoll`, { description,constituency});
     };
 
+    const addPoll = async (description) => {
+        return await axios.post(`${Api.API_BACKEND}/addpoll`, { description, constituency ,district :dist});
+    };
 
     return (
         <View style={styles.container}>
-            <Text style={{fontFamily:'Bold',marginBottom:10}}>Enter the Description</Text>
+            <Text style={{ fontFamily: 'Bold', marginBottom: 10 }}>Enter the Description</Text>
             <TextInput
                 style={styles.input}
                 placeholder="Enter poll description"
@@ -144,17 +205,55 @@ let userType=''
                 onChangeText={text => setPollDescription(text)}
             />
 
-            <TouchableOpacity  style={styles.button} onPress={() => pollvalidation() && handleAddPoll()}>
+            <TouchableOpacity style={styles.button} onPress={() => pollvalidation() && handleAddPoll()}>
                 <Text style={styles.buttonText}>Add Poll</Text>
             </TouchableOpacity>
-              
 
+            {userType!='admin' ? (
+                <View>
+                    <View style={{ marginTop: 10 }}>
+                <Text style={{ fontFamily: 'Bold', marginBottom: 10 }}>Select District </Text>
+                <Picker
+                    selectedValue={selectedDistrict}
+                    style={{ height: 50, width: 300 }}
+                    onValueChange={(itemValue) => setSelectedDistrict(itemValue)}
+                >
+                    <Picker.Item label="Select District" value="" />
+                    {districts.map((item, index) => (
+                        <Picker.Item key={index} label={item} value={item} />
+                    ))}
+                </Picker>
+            </View>
 
-            <FlatList
+            <View style={{ marginTop: 10 }}>
+                <Text style={{ fontFamily: 'Bold', marginBottom: 10 }}>Select Constituency </Text>
+                <Picker
+                    selectedValue={selectedConstituency}
+                    style={{ height: 50, width: 300 }}
+                    onValueChange={(itemValue) => setSelectedConstituency(itemValue)}
+                >
+                    <Picker.Item label="Select Constituency" value="" />
+                    {constituencies.map((item, index) => (
+                        <Picker.Item key={index} label={item} value={item} />
+                    ))}
+                </Picker>
+                <FlatList
+                data={filteredPoll}
+                renderItem={renderPollItem}
+                keyExtractor={(item) => item._id}
+            />
+            </View>
+
+                    </View>
+            ):(
+                <FlatList
                 data={poll}
                 renderItem={renderPollItem}
                 keyExtractor={(item) => item._id}
             />
+            )}
+
+            
         </View>
     );
 };
@@ -171,30 +270,35 @@ const styles = StyleSheet.create({
         borderWidth: 1,
         marginBottom: 10,
         paddingHorizontal: 10,
-        borderRadius:10
+        borderRadius: 10
     },
     pollItem: {
-    
         padding: 20,
         marginVertical: 10,
         backgroundColor: '#fff',
         borderRadius: 10,
-        elevation: 5, 
-        flexDirection:'row',
-        justifyContent:'space-between'
+        elevation: 5,
+        flexDirection: 'row',
+        justifyContent: 'space-between'
     },
-    button:{
-        justifyContent:'center',
-        alignItems:'center'
+    button: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginBottom:30
+        
 
-    },buttonText:{
-        backgroundColor:'#ccc',
-        padding:10,
-        borderRadius:10,
-        fontFamily:'Regular'
+
+    },
+    buttonText: {
+        backgroundColor: '#ccc',
+        padding: 10,
+        borderRadius: 10,
+        fontFamily: 'Regular',
+        width:setWidth(90),
+        textAlign:'center'
 
     }
-    
+
 });
 
 export default PollScreen;
