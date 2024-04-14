8d502d996528cc85589b0e4883d520cf0f6c67b0
Arun <arunarofficial00@gmail.com>
2024-04-14 17:16:19 +0530

    feat : added feature to view all veriifed and non verified users and also search option

--- src/screens/User.js
+++ src/screens/User.js
@@ -1,56 +1,67 @@
-import React from 'react';
-import { StyleSheet, View, FlatList, Text, Image } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, View, FlatList, Text, Image, TextInput } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
 
 const User = ({ navigation, route }) => {
     const { data } = route.params;
+    const [searchQuery, setSearchQuery] = useState('');
 
-    let x=0
-    
+    let x = 0;
 
-    data.map((item)=>{
-        if(item.verified){
-            x=x+1
+    data.map((item) => {
+        if (item.verified) {
+            x = x + 1;
         }
-    })
+    });
 
-    console.log(x)
+
+    const handleItemPress = (item) => {
+        navigation.navigate('verifyadmin', { item: item ,user:'politicaladmin'});
+    };
+    const filteredData = data.filter((item) =>
+        item.name.toLowerCase().includes(searchQuery.toLowerCase())
+    );
 
     const renderItem = ({ item }) => (
-       item.verified ? (
-            <View style={[styles.itemContainer,{backgroundColor:'#d0eeec'}]}>
-            <Image source={{ uri: item.profileImage }} style={styles.profileImage} />
-            <View style={styles.detailsContainer}>
-                <Text style={styles.name}>Name : {item.name}</Text>
-                <Text style={styles.detail}>Adhar Number : {item.aadharNo}</Text>
-                <Text style={styles.detail}>Email : {item.email}</Text>
-                <Text style={[styles.detail,{color:'red'}]}>Verified</Text>
-            </View>
-        </View>
-        ):(
-            <View style={[styles.itemContainer,{backgroundColor:'#ccc'}]}>
-            <Image source={{ uri: item.profileImage }} style={styles.profileImage} />
-            <View style={styles.detailsContainer}>
-                <Text style={styles.name}>Name : {item.name}</Text>
-                <Text style={styles.detail}>Adhar Number : {item.aadharNo}</Text>
-                <Text style={styles.detail}>Email : {item.email}</Text>
-                <Text style={[styles.detail,{color:'red'}]}>Not Verified</Text>
-
-            </View>
-        </View>
+        item.verified ? (
+            <TouchableOpacity style={[styles.itemContainer, { backgroundColor: '#d0eeec' }]}  onPress={() => handleItemPress(item)}>
+                <Image source={{ uri: item.profileImage }} style={styles.profileImage} />
+                <View style={styles.detailsContainer}>
+                    <Text style={styles.name}>Name : {item.name}</Text>
+                    <Text style={styles.detail}>Adhar Number : {item.aadharNo}</Text>
+                    <Text style={styles.detail}>Email : {item.email}</Text>
+                    <Text style={[styles.detail, { color: 'red' }]}>Verified</Text>
+                </View>
+            </TouchableOpacity>
+        ) : (
+            <TouchableOpacity style={[styles.itemContainer, { backgroundColor: '#ccc' }]}  onPress={() => handleItemPress(item)}>
+                <Image source={{ uri: item.profileImage }} style={styles.profileImage} />
+                <View style={styles.detailsContainer}>
+                    <Text selectable={true} style={styles.name}>Name : {item.name}</Text>
+                    <Text selectable={true} style={styles.detail}>Adhar Number : {item.aadharNo}</Text>
+                    <Text  selectable={true}style={styles.detail}>Email : {item.email}</Text>
+                    <Text style={[styles.detail, { color: 'red' }]}>Not Verified</Text>
+                </View>
+            </TouchableOpacity>
         )
-
     );
 
     return (
         <View style={styles.container}>
-            <View style={{justifyContent:'space-around',flexDirection:'row',padding:20}}>
-            <Text style={{fontFamily:'Italic'}}>Verified users : {x}</Text>
-            <Text style={{fontFamily:'Italic'}}>Non verified users : {data.length - x}</Text>
+            <View style={{ justifyContent: 'space-around', flexDirection: 'row', padding: 20 }}>
+                <Text style={{ fontFamily: 'Italic' }}>Verified users : {x}</Text>
+                <Text style={{ fontFamily: 'Italic' }}>Non verified users : {data.length - x}</Text>
             </View>
+            <TextInput
+                style={styles.searchInput}
+                placeholder="Search by name"
+                onChangeText={setSearchQuery}
+                value={searchQuery}
+            />
             <FlatList
-                data={data}
+                data={filteredData}
                 renderItem={renderItem}
-                keyExtractor={(item) => item.id}
+                keyExtractor={(item) => item._id}
             />
         </View>
     );
@@ -59,7 +70,7 @@ const User = ({ navigation, route }) => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        marginTop:55,
+        marginTop: 55,
         backgroundColor: '#fff',
         padding: 10,
     },
@@ -69,12 +80,13 @@ const styles = StyleSheet.create({
         marginBottom: 10,
         borderBottomWidth: 1,
         borderBottomColor: '#ccc',
-        paddingBottom: 10,
-        borderRadius:10
+        padding: 10,
+        borderRadius: 10,
+        
     },
     profileImage: {
-        width: 50,
-        height: 50,
+        width: 70,
+        height: 100,
         borderRadius: 25,
         marginRight: 10,
     },
@@ -82,15 +94,22 @@ const styles = StyleSheet.create({
         flex: 1,
     },
     name: {
-        fontSize: 25,
+        fontSize: 20,
         marginBottom: 5,
-        fontFamily:'Italic',
-        
+        fontFamily: 'Italic',
     },
     detail: {
-        fontSize: 16,
+        fontSize: 12,
         marginBottom: 3,
-        fontFamily:'Italic'
+        fontFamily: 'Italic',
+    },
+    searchInput: {
+        height: 40,
+        borderColor: 'gray',
+        borderWidth: 1,
+        paddingLeft: 10,
+        marginBottom: 20,
+        borderRadius:10
     },
 });
 
