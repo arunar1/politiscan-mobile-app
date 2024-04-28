d2cdf0f7fe107fb9c7019fe4e015e9fba6efd4d1
Arun <arunarofficial00@gmail.com>
2024-04-28 22:58:51 +0530

    commit

--- src/screens/FeedbackOfConsTituency.js
+++ src/screens/FeedbackOfConsTituency.js
@@ -3,12 +3,17 @@ import { StyleSheet, View } from 'react-native';
 
 const FeedbackOfConsTituency = () => {
     return (
-        <View>
+        <View style={styles.container}>
             
         </View>
     );
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container:{
+        paddingTop:30,
+        flex:1
+    }
+})
 
 export default FeedbackOfConsTituency;
