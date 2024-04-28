071476ea264cab75243ceea0d1efb4297c7a416c
Arun <arunarofficial00@gmail.com>
2024-04-28 23:19:31 +0530

    added the head section

--- src/screens/FeedbackOfConsTituency.js
+++ src/screens/FeedbackOfConsTituency.js
@@ -4,7 +4,8 @@ import { StyleSheet, View } from 'react-native';
 const FeedbackOfConsTituency = () => {
     return (
         <View style={styles.container}>
-            
+            <Text style={styles.head}>Review </Text>
+
         </View>
     );
 }
@@ -13,7 +14,19 @@ const styles = StyleSheet.create({
     container:{
         paddingTop:30,
         flex:1
-    }
+    },
+    head: {
+    fontSize: 22,
+    marginBottom: 20,
+    fontFamily:'Bold',
+    justifyContent:'center',
+    textAlign:'center',
+    backgroundColor:'#5a1f85',
+    paddingVertical:20,
+    borderRadius:5,
+    color:'white',
+    
+    },
 })
 
 export default FeedbackOfConsTituency;
